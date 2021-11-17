9f57acc070596791dd946a407fc9b7eb7ae13f62
dylanjwolff <wolffdy0@gmail.com>
2021-11-17 21:47:16 +0800

    docstrings

--- rvsc/node_setup.py
+++ rvsc/node_setup.py
@@ -1,3 +1,5 @@
+"""Automated Setup for Web3 Interaction with Local Node
+"""
 from solcx import compile_source
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
@@ -7,7 +9,11 @@ from . import solc_vm
 
 
 def web3_geth_setup():
+    """Gets a Web3 session with a local Geth node on the default port
 
+    Returns:
+        Web3: a web3 session object
+    """
     w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     w3.eth.default_account = w3.eth.accounts[0]
@@ -15,6 +21,15 @@ def web3_geth_setup():
 
 
 def compile_contracts(w3, fname):
+    """Compiles contracts in a file
+
+    Args:
+        w3 (Web3): a web3 session
+        fname (str): path to a solidity file 
+
+    Returns:
+        dict: A mapping of ID's to w3.eth.contract objects 
+    """
     contracts = open(fname).read()
     solc_vm.Solc(solidity_ast_tools.get_compiler_version(contracts)).install()
     compiled_sol = compile_source(contracts)
@@ -29,6 +44,7 @@ def compile_contracts(w3, fname):
 
 
 def constructZilliqaToken(w3, contracts, instrumented=False):
+    """Helper / example method to construct the ZilliqaToken contract"""
     contract = contracts["ZilliqaToken"]
     tx_hash = contract.constructor(w3.eth.accounts[0], 100).transact()
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
@@ -45,5 +61,6 @@ def constructZilliqaToken(w3, contracts, instrumented=False):
 
 
 def constructBuchiChecker(w3, contract):
+    """Helper / example method to construct the monitor contract"""
     tx_hash = contract.constructor().transact()
     return w3.eth.wait_for_transaction_receipt(tx_hash)
